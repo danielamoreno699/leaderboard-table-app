3d90f7e65a20d660ca5b9fd383d33bb687a8c0d1
Daniela Moreno <danielamoreno699@gmail.com>
2023-04-11 14:54:57 -0500

    Update html

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './style.css';
+// let endpoint = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/'; 
+// fetch('https://reqbin.com/echo/post/json', {
+//     method: 'POST',
+//     body: JSON.stringify({ "id": 78912 })
+// })
+//    .then(response => response.json())
+//    .then(response => console.log(JSON.stringify(response)))
